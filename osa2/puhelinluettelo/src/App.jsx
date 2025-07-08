41ad12262c8f493b2e681e07080e5763de36a822
naatti16 <148633784+anttitulenheimo@users.noreply.github.com>
2025-07-08 11:30:21 +0300

    osa 2 c ready

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,19 +1,25 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Filter from "./components/Filter.jsx";
 import PersonForm from "./components/PersonForm.jsx";
 import Persons from "./components/Persons.jsx";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [showFiltered, setShowFiltered] = useState('')
+
+  // Gets the data from the server
+  useEffect(() => {
+    axios
+        .get('http://localhost:3001/persons')
+        .then(response => {
+                setPersons(response.data)
+        })
+  }, []);
+
 
   const addPerson = (event) => {
       event.preventDefault()
