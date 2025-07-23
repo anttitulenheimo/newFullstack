4d327fa9c3009858b7bb2e354dc35834bdb0870b
naatti16 <148633784+anttitulenheimo@users.noreply.github.com>
2025-07-23 17:21:25 +0300

    psa 4 b ready

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,6 +1,6 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
-
+const {request, response} = require("express");
 
 blogsRouter.get('/', (request, response) => {
   Blog.find({}).then((blogs) => {
@@ -9,12 +9,45 @@ blogsRouter.get('/', (request, response) => {
 })
 
 
-blogsRouter.post('/', (request, response) => {
+blogsRouter.post('/', async (request, response, next) => {
   const blog = new Blog(request.body)
 
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
+  try {
+    const savedBlog = await blog.save()
+    response.status(201).json(savedBlog)
+  } catch (error) {
+    next(error)
+  }
 })
+
+blogsRouter.delete('/:id', async (request, response, next) => {
+  try {
+  await Blog.findByIdAndDelete(request.params.id)
+  response.status(204).end()
+  } catch (error) {
+    next(error)
+  }
+})
+
+
+blogsRouter.put('/:id', async (request, response, next) => {
+
+  try {
+  const { title, author, url, likes} = request.body
+  const updatedBlog = await Blog.findByIdAndUpdate(
+      request.params.id,
+      { title, author, url, likes },
+      {new: true, runValidators: true}
+  )
+    if (updatedBlog) {
+      response.json(updatedBlog)
+    } else {
+      response.status(404).end()
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
 
 module.exports = blogsRouter
--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -30,16 +30,125 @@ beforeEach(async () => {
     await blogObject.save()
 })
 
-test.only('Right amount of blogs are returned', async () => {
+test('Right amount of blogs are returned', async () => {
     const response = await api.get('/api/blogs')
 
     assert.strictEqual(response.body.length, initialBlogs.length)
 })
 
-test.only('id is not _id', async () => {
+test('id is not _id', async () => {
     const response = await api.get('/api/blogs')
     response.body.forEach(blog => {
         assert.notStrictEqual(blog.id, undefined, 'id should be defined') // Not equal
         assert.strictEqual(blog._id, undefined, '_id should not be defined') // Should be equal
     })
+})
+
+test('a blog  can be added', async() => {
+    const newBlog = {
+        title: 'randomBlogi',
+        author: 'minä',
+        url: 'www.testi.com',
+        likes: 0
+    }
+
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+    const titles = response.body.map(blog => blog.title)
+
+    assert.strictEqual(response.body.length, initialBlogs.length + 1)
+
+    console.assert(titles.includes('randomBlogi'))
+
+})
+
+
+test('the likes is 0 if not given', async() => {
+    const newBlog = {
+        title: 'randomBlogi',
+        author: 'minä',
+        url: 'www.testi.com'
+    }
+
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+
+    const addedBlog = response.body.find(blog => blog.title === 'randomBlogi')
+    assert.strictEqual(addedBlog.likes, 0)
+
+
+})
+
+test('blog without title or url is not added', async () => {
+    const newBlog = {
+        author: 'minä',
+        likes: 4
+    }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+
+  const response = await api.get('/api/blogs')
+
+  assert.strictEqual(response.body.length, initialBlogs.length)
+})
+
+test('Deletion of blog succeeds with 204 if id is valid', async () => {
+
+    const response = await api.get('/api/blogs')
+    const blogToDelete = response.body[0]
+
+    await api
+        .delete(`/api/blogs/${blogToDelete.id}`)
+        .expect(204)
+
+    const blogsAtEnd = await Blog.find({})
+
+    assert.strictEqual(blogsAtEnd.length, initialBlogs.length - 1)
+
+    const titles = blogsAtEnd.map(blog => blog.title)
+    assert(!titles.includes(blogToDelete.title))
+})
+
+
+test('Blog can be updated', async () => {
+
+    const response = await api.get('/api/blogs')
+    const blogToUpdate = response.body.find(blog => blog.title === 'uusiBlogi')
+
+    const updatedBlog = {
+        title: 'uusiBlogi',
+        author: 'vsCode Tester',
+        url: 'http://google.fi',
+        likes: 4
+    }
+
+    await api
+        .put(`/api/blogs/${blogToUpdate.id}`)
+        .send(updatedBlog)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+
+
+    const blogsAtEnd = await api.get('/api/blogs')
+    const updatedBlogInDb = blogsAtEnd.body.find(blog => blog.id === blogToUpdate.id)
+
+    assert.strictEqual(updatedBlogInDb.likes, 4)
+})
+
+
+after(async () => {
+  await mongoose.connection.close()
 })
